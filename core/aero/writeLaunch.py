ca1f4db983325f13be9072433d0c33a4fe8dbc4a
Cees Trouwborst <ceestrouwborst@gmail.com>
2015-03-08 10:54:30 +0100

    Recursive version of launch file writer, including namespaces and parameters

--- core/aero/writeLaunch.py
+++ core/aero/writeLaunch.py
@@ -1,19 +1,51 @@
 import xml.etree.cElementTree as ET
+import xml.dom.minidom
+import os
 from objects import *
 from helpers import *
+
 
 def write(configuration, filename="aeroworks.launch"):
     # Create root element <launch>
     root = ET.Element("launch")
-    for mod in configuration.modules_to_add:
-        for executable in mod.implementation.executables:
-            node = ET.SubElement(root, "node", pkg=executable.pkg, name=executable.id, type=executable.executable)
+    recursiveWrite(configuration, root)
+    f = open(filename, 'w')
+    f.write(prettify(root))
 
-    # Make this recursive?
-    for ns in configuration.namespaces:
-        ns_elem = ET.SubElement(root,"namespace", id=ns.id)
-        for mod in ns.modules_to_add:
-            for executable in mod.implementation.executables:
-                node = ET.SubElement(ns_elem, "node", pkg=executable.pkg, name=executable.id, type=executable.executable)
-    tree = ET.ElementTree(root)
-    tree.write(filename)
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = xml.dom.minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="\t")
+
+
+def recursiveWrite(configPart, rootElem):
+    # Loop through modules at current level
+    for mod in configPart.modules_to_add:
+        # Loop through executables for specific module
+        for executable in mod.implementation.executables:
+            node = ET.SubElement(rootElem, "node", pkg=executable.pkg, name=executable.id, type=executable.executable)
+            # Check if one of the parameters that are to be set are present in this executable
+            # Todo/problem: params are not defined at executable level, but at module level.
+            for paramSearch in mod.parameters_to_add[:]:
+                for paramList in executable.params[:]:
+                    if paramSearch.name == paramList.name:
+                        param = ET.SubElement(node, "param", name=paramSearch.name, value=paramSearch.value)
+                        # Remove found param from both lists
+                        mod.parameters_to_add.remove(paramSearch)
+                        executable.params.remove(paramList)
+            # At this point, we also have the default parameters we should fill. loop again.
+            for paramList in executable.params:
+                param = ET.SubElement(node, "param", name=paramList.name, value=paramList.default)
+        # At this point, we end up with some parameters that are not "connected". Echo those.
+        for paramSearch in mod.parameters_to_add:
+            print 'Parameter "' + paramSearch.name + '", valued "' + paramSearch.value + '", could not be connected.'
+
+    try:
+        for ns in configPart.namespaces:
+            ns_elem = ET.SubElement(rootElem, "namespace", id=ns.id)
+            recursiveWrite(ns, ns_elem)
+    except AttributeError:
+        print 'No namespaces in configPart'
