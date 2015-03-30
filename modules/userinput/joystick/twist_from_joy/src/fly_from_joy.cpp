58464e7abd4143f4d4a47f4f1a6cb990f9e9c255
Cees Trouwborst <ceestrouwborst@gmail.com>
2015-03-30 15:02:23 +0200

    Fixed internal connections (order of remaps).

--- modules/userinput/joystick/twist_from_joy/src/fly_from_joy.cpp
+++ modules/userinput/joystick/twist_from_joy/src/fly_from_joy.cpp
@@ -102,9 +102,9 @@ public:
     // Init subscribers
     joy_subscriber_ = node_handle_.subscribe<sensor_msgs::Joy>("joy", 1, boost::bind(&Teleop::joyCallback, this, _1));
     velocity_publisher_ = node_handle_.advertise<geometry_msgs::Twist>("cmd_vel_joy", 1);
-    land_publisher_ = node_handle_.advertise<std_msgs::Empty>("ardrone/land", 1);
-    takeoff_publisher_ = node_handle_.advertise<std_msgs::Empty>("ardrone/takeoff", 1);
-    reset_publisher_ = node_handle_.advertise<std_msgs::Empty>("ardrone/reset",1);
+    land_publisher_ = node_handle_.advertise<std_msgs::Empty>("land", 1);
+    takeoff_publisher_ = node_handle_.advertise<std_msgs::Empty>("takeoff", 1);
+    reset_publisher_ = node_handle_.advertise<std_msgs::Empty>("reset",1);
   }
 
   ~Teleop()
