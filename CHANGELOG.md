ead69a0ad2a678daedb59897a7e2eba11ecbec94
Robin Hoogervorst <robin9975@gmail.com>
2016-11-21 09:41:11 +0100

    changed changelog and versioning

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 0.2.1
+
+- Fix for #13:
+    - Removed VIKI launch file, as this is auto-generated
+    - Bang line on top, before the command
+    - Parameter added such that the desktop file command executes succesfully
--- viki_launch.sh
+++
@@ -1,8 +0,0 @@
-# VIKI: more than a GUI for ROS, https://github.com/UT-RAM/viki 
-# version: 0.2 - Alice
-# copyright: Robin Hoogervorst, Alex Kamphuis, Cees Trouwborst, 2016 
-# licensed under the MIT License
-
-#!/bin/bash
-. /mnt/data/RAM/workspace/src/viki/viki_env.sh
-exec "$@"
